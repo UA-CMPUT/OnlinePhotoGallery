e2fba5aed7dc79867e7d864b3d635257bd206b62
Yueran Sun <yueran1@cs.ualberta.ca>
2016-03-21 23:38:06 -0600

    Finish dataAnalysis html, still working on dataAnalysis.php

---
+++ Bo/dataAnalysis.php
@@ -0,0 +1,27 @@
+<!DOCTYPE DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Data Analysis</title>
+</head>
+
+
+<?php
+	include ("connDB.php");
+	//establish connection
+	$conn=connect();
+?>
+
+
+
+
+
+
+
+
+
+
+</html>
+
